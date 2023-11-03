50cedb7f6bddc37e34cef9ec059650fe5e82404b
DigitalZeus <65643207+DigitalZeus@users.noreply.github.com>
2023-11-03 19:33:43 +0530

    first react native app

---
+++ README.md
@@ -0,0 +1,14 @@
+
+## Introduction
+The existence of React Native makes React one of the most valuable skills to have.
+
+Alongside building this application, I also learned how to:
+- develop visually appealing UI/UX designs
+- fetch data from an external API and integrate it
+- implement search & pagination functionality
+- create customAPI data fetching hooks
+- write clean, organized, and maintainable code with proper architecture
+- follow the best practices for React Native development
+
+needs to be installed -g expo-cli
+start code expo-cli start --tunnel
---
+++ hook/useFetch.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      // 'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+      'X-RapidAPI-Key': '5b6cbad58cmsh3c7b3f1c3f57b9dp160a50jsnb4a26025a3ae',
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      console.log(response.data.data)
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      // console.log(error)
+      setError(error);
+      console.log(error)
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
